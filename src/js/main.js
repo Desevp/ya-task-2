0a13ca78c6f0a16e075a95ddcbcf8b82b400ce35
Jane Des <desevp@gmail.com>
2018-07-26 13:12:30 +0300

    [WIP] Changed slider js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
+  @@include('partials/SimpleSlider.js')
 
-    @@include('partials/SimpleSlider.js')
-
+  let scriptSlider = new SimpleSlider('.js-script-slider', {
+    wrapper: '.script-slider__inner',
+    item: '.script-slider__item',
+    prevBtn: '.js-script-slider-prev',
+    nextBtn: '.js-script-slider-next'
+  });
 });
 
 window.onload = function() {
--- src/js/partials/simpleSlider.js
+++ src/js/partials/simpleSlider.js
@@ -1,5 +1,4 @@
 class SimpleSlider {
-
   constructor(element, options = {}) {
     this.slider = document.querySelector(element);
     this.initialized = false;
@@ -7,41 +6,41 @@ class SimpleSlider {
     this.options  = Object.assign({
       wrapper: '.devices-swiper__inner',
       item: '.test2__item',
-      controls: '.devices-swiper__arrows',
+      marginItem: 15,
       prevBtn: '.devices-swiper__arrows-prev',
-      nextBtn: '.devices-swiper__arrows-next'
+      nextBtn: '.devices-swiper__arrows-next',
+      btnDisablesClass: 'slider-arrows__btn--is-disabled'
     }, options);
 
     if (!( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )) {
       this.init();
     }
 
-    // window.addEventListener('resize', () => {
-    //   if ((document.body.clientWidth > 768) && !(this.initialized)) {
-    //     this.init();
-    //   }
-    //
-    //   if ((document.body.clientWidth < 768) && (this.initialized)) {
-    //     this.disable();
-    //   }
-    // });
+    window.addEventListener('resize', () => {
+      if ((document.body.clientWidth > 1024) && !(this.initialized)) {
+        this.init();
+        this.wrapper.style.transform = 'translateX(0px)';
+        this._setDefaultArrow();
+      }
+
+      if ((document.body.clientWidth <= 1024) && (this.initialized)) {
+        this.disable();
+      }
+    });
   }
 
   init () {
-
     this.initialized = true;
     this.wrapper = this.slider.querySelector(this.options.wrapper);
     this.item = this.slider.querySelector(this.options.item);
     this.prevBtn = document.querySelector(this.options.prevBtn);
     this.nextBtn = document.querySelector(this.options.nextBtn);
 
-    this.widthItem = this.item.offsetWidth;
-
-    this.itemsLenght = this.slider.querySelectorAll(this.options.item).length;
-
-    this.marginItem = 15;
+    this.marginItem = this.options.marginItem;
 
     this.slider.style.overflowX = 'hidden';
+
+    this._setDefaultArrow();
 
     this.nextBtn.addEventListener('click', (event) => {
       if(!event.detail || event.detail == 1){
@@ -54,6 +53,11 @@ class SimpleSlider {
         this.move('prev');
       }
     }
+  }
+
+  _setDefaultArrow() {
+      this.prevBtn.classList.add(this.options.btnDisablesClass);
+      this.nextBtn.classList. remove(this.options.btnDisablesClass);
   }
 
   disable() {
@@ -69,11 +73,12 @@ class SimpleSlider {
     }
 
     let _getCountItemOffset = () => {
-      return Math.floor(this.wrapper.offsetWidth/this.item.offsetWidth);
+      return Math.floor(this.slider.offsetWidth/this.item.offsetWidth);
     }
 
     let curTranslate = _getTranslateX(this.wrapper);
-    let offset = _getCountItemOffset() * (this.widthItem + this.marginItem);
+
+    let offset = _getCountItemOffset() * (this.item.offsetWidth + this.marginItem);
 
     let newVal;
 
@@ -82,24 +87,28 @@ class SimpleSlider {
       if (newVal > 0) {
         newVal = 0;
       }
+      else {
+        this.nextBtn.classList. remove(this.options.btnDisablesClass);
+      }
+
+      if ((newVal + offset) > 0) {
+        this.prevBtn.classList. add(this.options.btnDisablesClass);
+      }
     }
     else {
       newVal = curTranslate - offset;
+
       if (Math.abs(newVal) > this.wrapper.scrollWidth) {
         newVal = curTranslate;
+      }
+      else {
+        this.prevBtn.classList. remove(this.options.btnDisablesClass);
+      }
+
+      if (Math.abs(newVal - offset) > this.wrapper.scrollWidth) {
+        this.nextBtn.classList. add(this.options.btnDisablesClass);
       }
     }
     this.wrapper.style.transform = 'translateX('+ newVal +'px)';
   }
 }
-
-let test = new SimpleSlider('.devices-swiper', {
-
-});
-
-let test2 = new SimpleSlider('.test-slider', {
-  wrapper: '.test',
-  item: '.test__item',
-  prevBtn: '.prev',
-  nextBtn: '.next'
-});
